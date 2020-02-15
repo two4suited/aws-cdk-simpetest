acf4568dcfae302bac844ac877dd28f337b1bbda
Brian Sheridan <brian.sheridan@gmail.com>
2020-02-15 12:02:16 -0800

    change

--- infrastructure/src/Infrastructure/InfrastructureStack.cs
+++ infrastructure/src/Infrastructure/InfrastructureStack.cs
@@ -6,7 +6,7 @@ namespace Infrastructure
     {
         internal InfrastructureStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
-            // The code that defines your stack goes here
+            //  The code that defines your stack goes here
         }
     }
 }
